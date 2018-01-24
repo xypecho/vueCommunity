9e15be86cf22d72381c89b3a913ec33133981e65
xypecho <xypecho@163.com>
2018-01-24 22:36:02 +0800

    Signed-off-by: xypecho <xypecho@163.com>

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted(){
+    this.getData();
+  },
+  methods:{
+    getData:function(){
+      this.$http({
+        methods:'get',
+        url:'https://www.vue-js.com/api/v1/topics',
+        params:{
+          page:1,
+          tab:'ask',
+          limit:20,
+          mdrender:true
+        }
+      }).then(function(data){
+        console.log(data);
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
